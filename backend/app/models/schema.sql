65e9cc2941eccb0a19f2d8efcab722d4ca8265c0
nitesh kakkar <kakkar.nitesh04@gmail.com>
2025-02-08 23:21:53 +0530

    feat: created trigger in db to compute order value

--- backend/app/models/schema.sql
+++ backend/app/models/schema.sql
@@ -14,8 +14,7 @@ CREATE TABLE IF NOT EXISTS menu (
 CREATE TABLE IF NOT EXISTS orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    item_id INT,
-    quantity INT DEFAULT 1,
+    order_value DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (menu_id)
@@ -29,4 +28,25 @@ CREATE TABLE IF NOT EXISTS order_items (
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
     FOREIGN KEY (item_id) REFERENCES menu(id) ON DELETE CASCADE
 );
+
+DELIMITER //
+
+CREATE TRIGGER update_order_value
+AFTER INSERT ON order_items
+FOR EACH ROW
+BEGIN
+    UPDATE orders
+    SET order_value = (
+        SELECT SUM(menu.price * order_items.quantity)
+        FROM order_items
+        JOIN menu ON order_items.item_id = menu.id
+        WHERE order_items.order_id = NEW.order_id
+    )
+    WHERE id = NEW.order_id;
+END;
+
+//
+
+DELIMITER ;
+
 
