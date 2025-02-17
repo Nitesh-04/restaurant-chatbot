6cfe6ffc3ea5848b4921d5639e72ee0763e88be4
nitesh kakkar <kakkar.nitesh04@gmail.com>
2025-02-17 23:00:21 +0530

    feat: intg of client menu and cart with backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import users, orders, menu ,cart, track, webhook
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(orders.router, prefix="/orders", tags=["Orders"])
